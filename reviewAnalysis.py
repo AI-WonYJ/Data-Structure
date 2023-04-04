ab19cc898f684a709ffccda5a049187757dfb0a2
YoungJin <101448204+AI-WonYJ@users.noreply.github.com>
2023-04-04 23:57:59 +0900

    Update reviewAnalysis.py

--- reviewAnalysis.py
+++ reviewAnalysis.py
@@ -1,38 +1,23 @@
 import pandas as pd
 from circularDoublyLinkedList import *
 from circularDoublyLinkedListFilter import *
-
-def show_review_
   
 def load_data(dataset: CircularDoublyLinkedList):
+  i = 0
   with open('amazon_reviews_us_PC_v1_00.tsv/amazon_reviews_us_PC_v1_00.tsv', 'r') as file:
-    s = 1
-    end = 10
     for line in file:
       fileds = line.split('\t')
-      print(fileds)
-      s += 1
-      if s >= end:
+      if i != 0:
+        dataset.append(i, fileds[7])
+      i += 1
+      if i == 11001:
         break
 
-def get_data(dataset: CircularDoublyLinkedList):
-  filtered = dataset.filter(CircularDoublyLinkedListFilter())
-
-a = CircularDoublyLinkedListFilter()
-a.load_data()
-
-# ['marketplace',
-# 'customer_id',
-# 'review_id',
-# 'product_id',
-# 'product_parent',
-# 'product_title',
-# 'product_category',
-# 'star_rating',
-# 'helpful_votes',
-# 'total_votes',
-# 'vine',
-# 'verified_purchase',
-# 'review_headline',
-# 'review_body',
-# 'review_date\n']
+def get_data(dataset: CircularDoublyLinkedList, value):
+  filtered = dataset.filter(dataset, value)
+  print(filtered)
+  
+if __name__ == '__main__':
+  dataset = CircularDoublyLinkedList()
+  load_data(dataset)
+  get_filter_list = get_data(dataset, '1')
