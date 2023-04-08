74b5851b45c22812858f5214e3ffcd43e1f3cedf
YoungJin <101448204+AI-WonYJ@users.noreply.github.com>
2023-04-08 23:55:12 +0900

    Update circularDoublyLinkedListFilter.py

--- circularDoublyLinkedListFilter.py
+++ circularDoublyLinkedListFilter.py
@@ -1,10 +1,17 @@
-from circularDoublyLinkedList import *
-
 class CircularDoublyLinkedListFilter:
-  def __init__(self):
-    self.item = ""
-    self.value = ""
-  
-  def filter(self, standard):
-    self.filter_content = standard
+  def __init__(self, sheet, value):
+    self.__head = sheet.getNode(-1)
+    self.loc = self.__head.next
+    self.value = value
     
+  def find(self, value):
+    curr = self.loc
+    filter_list = []
+    while curr != self.__head:
+      if curr.item[1] == value:
+        filter_list.append(curr.item)
+      curr = curr.next
+    # print(filter_list)
+    return filter_list
+  
+  
