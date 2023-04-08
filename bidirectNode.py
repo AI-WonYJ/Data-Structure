99db07e9ba8b94358bfb8df2d9f12c8fc45ad6e7
YoungJin <101448204+AI-WonYJ@users.noreply.github.com>
2023-04-08 23:55:37 +0900

    Update bidirectNode.py

--- bidirectNode.py
+++ bidirectNode.py
@@ -1,5 +1,19 @@
 class BidirectNode:
-  def __init__(self, x, prevNode: 'BidirectNode', nextNode:'BidirectNode'):
-    self.item = x
+  def __init__(self, key, value, prevNode: 'BidirectNode', nextNode:'BidirectNode'):
+    self.key = key
+    self.value = value
+    self.item = [self.key, self.value]
     self.prev = prevNode
     self.next = nextNode
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
