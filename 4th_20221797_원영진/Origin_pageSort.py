c6b3dea49607873d176166599d39beb8b85d7334
17Z95N-GP75ML <yjason7587@soongsil.ac.kr>
2023-05-09 17:26:41 +0900

    4th

---
+++ 4th_20221797_원영진/Origin_pageSort.py
@@ -0,0 +1,31 @@
+from Modify_quickSort import *
+from Origin_mergeSort import *
+
+def do_sort(input_file):
+  
+  data_file = open(input_file)
+  A = []
+  data_dict = {}
+  for line in data_file.readlines():
+    lpn = line.split()[0]
+    if lpn not in data_dict.keys():
+      data_dict[lpn] = len(data_dict)
+      A.append([lpn, 1])
+    else:
+      A[data_dict.get(lpn)][1] += 1
+    
+  for i in range(0, len(A)):
+    if A[i][0] == '0':
+      print(A[i])
+    # print(A[i], end = " ")
+    # print("")
+  # quickSort(A, 0, len(A)- 1)
+  # print("DID")
+  # mergeSort(A, 0 , len(A)-1)
+  
+  for i in range(10):
+    print(A[i], end=" ")
+    print("")
+  
+if __name__ == "__main__":
+  do_sort("4th_20221797_원영진/linkbench_short.trc")
