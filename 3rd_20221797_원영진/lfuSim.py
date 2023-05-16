bb26af5e25de73d6cd02069fa2483f4a4be4d88f
AI-WonYJ <yjason7587@naver.com>
2023-05-16 22:00:14 +0900

    Add: 과제 2

--- 3rd_20221797_원영진/lfuSim.py
+++ 3rd_20221797_원영진/lfuSim.py
@@ -1,35 +1,45 @@
 from Modify_heap import *
 
 def lfu_sim(cache_slots):
-  data_dict = {}
   data_list = []
+  data_dict = {}
+  cache_heap = MinHeap()
   cache_hit = 0
   tot_cnt = 0
 
-  data_file = open("linkbench.trc")
+  data_file = open("3rd_20221797_원영진/linkbench.trc")
 
   for line in data_file.readlines():
     lpn = line.split()[0]
+    data_list.append(lpn)
 
-    # Program het
-    if len(data_dict) < cache_slots:
-        if lpn in data_dict.keys():
-            data_list[data_dict.get(lpn)][1] += 1
-        else:
-            data_dict[lpn] = len(data_dict)
-            data_list.append([lpn, 1])
+  for i in range(0, len(data_list)):
+    tot_cnt += 1
+    data = data_list[i]
+
+    if cache_heap.size() < cache_slots:
+      if data in data_dict.keys():
+        data_dict[data] += 1
+        cache_hit += 1
+        cache_heap.UpdateHeap(data)
+      else:
+        data_dict[data] = 1
+        cache_heap.Insert([data, 1])
+
     else:
-       if lpn in data_dict.keys():
-          data_list[data_dict.get(lpn)][1] += 1
-       else:          
-          heapsort = Heap(data_list)
-          heapsort.buildHeap()
-          heapsort.delete
-          del data_dict[lpn]
-      
-    
+      if data in data_dict.keys():
+        if cache_heap.CacheIn(data) == True:
+          data_dict[data] += 1
+          cache_hit += 1
+          cache_heap.UpdateHeap(data)
+        else:
+          data_dict[data] += 1
+          cache_heap.UpdateMin([data, data_dict[data]])
+      else:
+        data_dict[data] = 1
+        cache_heap.UpdateMin([data, 1])
 
-    print("cache_slot = ", cache_slots, "cache_hit = ", cache_hit, "hit ratio = ", cache_hit / tot_cnt)
+  print("cache_slot = ", cache_slots, "cache_hit = ", cache_hit, "hit ratio = ", cache_hit / tot_cnt)
 
 if __name__ == "__main__":
   for cache_slots in range(100, 1000, 100):
